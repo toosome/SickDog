586ecdd78e272bd839972f9f10f6ed725faa48bf
unknown <630367039@qq.com>
2021-02-05 00:48:06 +0800

    Lesson1
    第一节课

---
+++ SickDog-OpenWorld/Assets/Scenes/MyCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour
+{
+    private Transform cameraPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraPos = Dage.Instance.transform.Find("cameraPos");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //lerp函数实现镜头跟随
+
+      //  transform.position = cameraPos.position;
+       // transform.rotation = cameraPos.rotation;
+
+        transform.position =Vector3.Lerp(transform.position,cameraPos.position,0.2f);
+        transform.rotation = Quaternion.Lerp(transform.rotation, cameraPos.rotation,0.2f);
+    }
+}
